229734c127b492f73af84d2ec5babd015e744a2a
Kaique <kiaqbs@gmail.com>
2025-03-02 11:27:52 -0300

    Initial commit

---
+++ assets/js/scripts.js
@@ -0,0 +1,8 @@
+function showNavbar() {
+  const btnNav = document.getElementById("btn-nav");
+  const navbar = document.getElementById('navbar');
+
+  btnNav.classList.toggle("show-nav");
+  navbar.classList.toggle("active-nav");
+}
+
