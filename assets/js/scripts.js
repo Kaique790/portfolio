c6dd73f7969527f86982f7d2fe0f7d5336908092
Kaique790 <kiaqbs@gmail.com>
2025-03-09 14:16:42 -0300

    Responsividade

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,8 +1,47 @@
+const btnNav = document.getElementById("btn-nav");
+const navbar = document.getElementById('navbar');
+const logoSkillsSection = document.querySelector('.logo-skills');
+
 function showNavbar() {
-  const btnNav = document.getElementById("btn-nav");
-  const navbar = document.getElementById('navbar');
 
   btnNav.classList.toggle("show-nav");
   navbar.classList.toggle("active-nav");
 }
+
+let scrollSpeed = 1;
+let isReturning = false;
+let scrollInterval;
+
+function scrollSection() {
+  if (!isReturning) {
+    logoSkillsSection.scrollLeft += scrollSpeed;
+
+    if (logoSkillsSection.scrollLeft >= logoSkillsSection.scrollWidth - logoSkillsSection.clientWidth) {
+      isReturning = true;
+
+      logoSkillsSection.scrollTo({
+        left: 0,
+        behavior: 'smooth',
+      });
+
+      setTimeout(() => {
+        isReturning = false;
+      }, 1000);
+    }
+  }
+}
+
+function startScrolling() {
+  scrollInterval = setInterval(scrollSection, 10);
+}
+
+function stopScrolling() {
+  clearInterval(scrollInterval);
+}
+
+logoSkillsSection.addEventListener('mouseover', stopScrolling);
+logoSkillsSection.addEventListener('mouseout', startScrolling);
+
+startScrolling();
+
 
